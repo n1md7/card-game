27a782943f2b4d712d9f96d2dd6ee2536b260af4
fifaia <36516921+fifaia@users.noreply.github.com>
2020-08-09 18:20:59 +0400

    markup #9
    change 1

---
+++ markup/game.js
@@ -0,0 +1,12 @@
+
+
+function AddCardTable(cardnumber = 0){
+	var mintop = 20;
+	var maxtop = 200;
+	var minleft = 45;
+	var maxleft = 540;
+
+	for (var i = 0; i < cardnumber.lenght; i++) {
+		
+	}
+}
