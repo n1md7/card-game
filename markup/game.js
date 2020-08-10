33da865f999663b99add71c36a8f93a95934046d
fifaia <36516921+fifaia@users.noreply.github.com>
2020-08-10 13:17:44 +0400

    markupchange3 #9
    cards names

--- markup/game.js
+++ markup/game.js
@@ -4,12 +4,13 @@ function getRandom(min, max){
 
 
 function AddCardTable(cardnumber = 0){
+	var cardsname = ['SpadesKing', 'HeartsKing', 'HeartsJack', 'Diamonds_9', 'Clubs_5'];
 	$('.board-cards').empty();
 	for (var i = 0; i < cardnumber; i++) {
 		var top = getRandom(20, 200);
 		var left = getRandom(45, 540);
 		var rotation = getRandom(0, 45);
-		$('.board-cards').append('<div class="card-pl bg table-'+(i+1)+' active tb-card"></div>');
+		$('.board-cards').append('<div class="card-pl bg table-'+(i+1)+' '+cardsname[getRandom(0, cardsname.length)]+' tb-card"></div>');
 		$('.table-'+(i+1)).css('top', top +'px');
 		$('.table-'+(i+1)).css('left', left +'px');
 		$('.table-'+(i+1)).css('transform', 'rotate('+rotation+'deg)');
