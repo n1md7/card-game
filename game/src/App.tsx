76e08b08938c0838859ba39f8ded463373198450
gtetvadze <g_tetvadze@cu.edu.ge>
2020-08-11 23:19:40 +0400

    grid filter added

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -8,16 +8,17 @@ const testData = {
   grid: [
     {id: 111, players: 2},
     {id: 211, players: 2},
-    {id: 3222, players: 2}
+    {id: 322, players: 1},
+    {id: 347, players: 3},
+    {id: 3278, players: 7}
   ]
 }
-
 
 function App() {
   return (
       <>
         <Header/>
-        <Grid data={testData.grid}/>
+        <Grid data={testData.grid} filters={Object.fromEntries(Object.keys(testData.grid[0]).map(a => [a, '']))}/>
         <Footer/>
       </>
   );
