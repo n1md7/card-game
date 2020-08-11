291ca4eabbcc5035b961db1af677cf03f457bfee
gtetvadze <g_tetvadze@cu.edu.ge>
2020-08-11 23:36:20 +0400

    grid filters

--- game/src/components/Grid.tsx
+++ game/src/components/Grid.tsx
@@ -2,37 +2,59 @@ import React, {useState} from 'react';
 import GridCell from "./GridCell";
 import './../css/grid.css';
 
+interface FilterValueProps {
+    id: string;
+    players: string;
+}
+
 const Grid = (props: any) => {
+
     const clickHandler = (id: number) => console.log(id);
-    const [filterValue, setFilterValue] = useState('');
+
+    const [filterValue, setFilterValue] = useState<any>({...props.filters});
+
     const [data, setData] = useState([...props.data]);
 
-    const onFilter = (event: any) => {
-        setFilterValue(event.target.value);
-        setData(props.data.filter((el: any) => el.id.toString().startsWith(event.target.value)));
+    const onFilter = (event: any, dataKey: string) => {
+        event.preventDefault();
+        setFilterValue({...filterValue, [dataKey]:event.target.value});
+        let tempFilter = {...filterValue, [dataKey]:event.target.value};
+        let tempData = props.data;
+        for(let key in tempFilter) {
+            if(tempFilter.hasOwnProperty(key)) {
+                tempData = tempData.filter((el: any) => el[key].toString().includes(tempFilter[key]));
+            }
+        }
+        setData(tempData);
     }
 
     return (
-        <>
-            <input
-                className={'grid-filter'}
-                value={filterValue}
-                onChange={onFilter}
-            />
-            <div className={'grid'}>
-                {
-                    data
-                        .map(
-                            (cellData: any) =>
-                                <GridCell
-                                    key={cellData.id}
-                                    data={cellData}
-                                    clickHandler={clickHandler}
-                                />
-                        )
-                }
-            </div>
-        </>
+        <div className={'grid'}>
+            {
+                props.data.length > 0 && Object.keys(props.data[0]).map(
+                    (dataKey: string) =>
+                        <input
+                            key={dataKey}
+                            className={'grid-filter'}
+                            value={filterValue[dataKey]}
+                            onChange={(event) => onFilter(event, dataKey)}
+                        />
+                )
+            }
+            {
+                data.length === 0 ?
+                    (<div>No data found!!!</div>) :
+                    data.map(
+                        (cellData: any) =>
+                            <GridCell
+                                key={cellData.id}
+                                data={cellData}
+                                clickHandler={clickHandler}
+                            />
+                    )
+            }
+        </div>
+
     )
 }
 
