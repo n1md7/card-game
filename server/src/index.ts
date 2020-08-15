e88c0b4863b9a16eb47515989bbbde314f458971
gtetvadze <g_tetvadze@cu.edu.ge>
2020-08-15 21:09:41 +0400

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,56 +1,19 @@
-import http from "http";
-import Koa from "koa";
-import Router from "koa-router";
-import socketIO from "socket.io";
-import dotenv from "dotenv";
-import serve from "koa-static";
-import path from "path";
-import Cors from '@koa/cors'
-import {SocketCallback} from "./types";
-// initialize configuration
-dotenv.config();
-
-// port is now available to the Node.js runtime
-// as if it were an environment variable
-const httpPort = process.env.SERVER_PORT_HTTP;
-
-const app = new Koa();
-const router = new Router();
-app.use(Cors());
-
-const httpServer = http.createServer(app.callback());
-// Pass a http.Server instance to the listen method
-const io = socketIO.listen(httpServer);
-
-app.use(router.routes())
-    .use(router.allowedMethods())
-    .use(async (ctx, next) => {
-        // custom middleware
-        ctx.cookies.set('test-cookie', 'test-value');
-        await next();
-    });
-
-// make public all content inside ../public folder
-// mainly for testing(socket.io),
-// public files will be served from apache/nginx
-app.use(serve(path.join(__dirname, '../public')));
+import {router} from "./httpServer";
+import gameSetup from "./controller/gameSetup";
 
 // To check server status
-router.get('/status-check', ctx => ctx.body = {
-    status: "up"
-});
+router.get('/status-check', ctx => gameSetup.status(ctx));
+router.get('/authenticate/:name?', ctx => gameSetup.authenticate(ctx));
+router.get('/create-room/:size/:isPublic', ctx => gameSetup.create(ctx));
+router.get('/show-rooms', ctx => gameSetup.showRooms(ctx));
+router.get('/join-room/:id', ctx => gameSetup.joinRoom(ctx));
+router.get('/leave-room', ctx => gameSetup.leaveRoom(ctx));
 
-io.on('connection', function (socket: any) {
-    console.log("Connected successfully to the socket ...");
-    socket.on('create', function (message: any) {
-        console.log(message);
-    });
-    socket.on('join', function (message: any) {
-        console.log(message);
-    });
-});
+router.get('/lol/:id?', ctx => {
+    console.dir(ctx.response.body);
+    console.dir(ctx.params);
 
-// start the server
-httpServer.listen(httpPort, () => {
-    console.log(`server started at http://localhost:${httpPort}`);
+    ctx.body = {
+        yo: 'got it'
+    };
 });
