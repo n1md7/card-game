5c88e4a5ecf2ec84d01ebaf0240cd0c2698f1107
bichiko <b.kodua@gmail.com>
2020-08-17 17:29:36 +0300

    Test improvement for helper Array proto

--- server/src/__tests__/unit/helpers/index.test.js
+++ server/src/__tests__/unit/helpers/index.test.js
@@ -1,15 +1,15 @@
 require('../../../helpers/index');
 
 describe('Test helper functions', () => {
-    it('should remove an element from the list',  () => {
-        const list = [1,2,34,5];
+    it('should remove an element from the list', () => {
+        const list = [1, 2, 34, 5];
         const previousLength = list.length;
         list.remove(2);
         const currentLength = list.length;
         expect(previousLength).not.toBe(currentLength);
     });
 
-    it('should remove an element from the strings list',  () => {
+    it('should remove an element from the strings list', () => {
         const list = ['hey', 'you'];
         list.remove('you');
         expect(list.length).toBe(1);
@@ -17,4 +17,18 @@ describe('Test helper functions', () => {
         // still should be one
         expect(list.length).toBe(1);
     });
-})
+});
+
+describe.each([
+    [[1, 2, 3, 4], 1, 3],
+    [[1, 2, 3], 1, 2],
+    [[1, 2], 1, 1],
+    [[1, 2], 3, 2],
+    [[1], 1, 0],
+    [[], 1, 0],
+])('Array.remove(%p, %i)', (list, remove, expected) => {
+    it(`returns list len after remove operation which is: ${expected}`, () => {
+        list.remove(remove);
+        expect(list.length).toBe(expected);
+    });
+});
