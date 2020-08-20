bea5c5eb7e63b0accc97b585467bed6dec4c5bee
bichiko <b.kodua@gmail.com>
2020-08-20 16:51:37 +0300

    added webstorm config file to have the same code styles

--- game/src/components/room/Join.jsx
+++ game/src/components/room/Join.jsx
@@ -1,9 +1,9 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import RoomsTableList from "./Rooms";
 
 export default () => (
-    <Fragment>
-        <h3 className="text-center mt-5 mb-4 text-muted">Rooms</h3>
-        <RoomsTableList/>
-    </Fragment>
+  <Fragment>
+    <h3 className="text-center mt-5 mb-4 text-muted">Rooms</h3>
+    <RoomsTableList/>
+  </Fragment>
 );
--- game/src/components/room/Room.jsx
+++ game/src/components/room/Room.jsx
@@ -1,29 +1,29 @@
-import React, {useEffect} from "react";
-import {useParams} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {updateUser} from "../../redux/actions";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { updateUser } from "../../redux/actions";
 
 // id is prop passed from Lobby
 // when redirect happens prop id is null
-export default function Room({id}) {
-    // roomId is URL route param
-    const {roomId} = useParams();
-    const dispatch = useDispatch();
+export default function Room( { id } ) {
+  // roomId is URL route param
+  const { roomId } = useParams();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        // when accessed directly from the URL
-        if (!id) {
-            // get roomID from the store and dispatch it
-            dispatch(updateUser({
-                roomId
-            }));
-        }
-    }, []);
+  useEffect( () => {
+    // when accessed directly from the URL
+    if ( !id ) {
+      // get roomID from the store and dispatch it
+      dispatch( updateUser( {
+        roomId
+      } ) );
+    }
+  }, [] );
 
-    return roomId ? (
-        <div>This is your game room {roomId}</div>
-    ) : (
-        <h3>Requested room ID: {roomId} does not exist!</h3>
-    )
+  return roomId ? (
+    <div>This is your game room { roomId }</div>
+  ) : (
+    <h3>Requested room ID: { roomId } does not exist!</h3>
+  )
 
 }
