42baf1e55593c04b2a41beb17cb7f6977307a69b
n1md7 <harry.kodua@rangeforce.com>
2023-03-16 01:07:00 +0200

    chore: env types

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -1,9 +1,12 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: string;
-    JWT_SECRET: string;
-    SERVER_PORT: string;
-    APP_PORT: string;
-    ORIGINS: string;
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      JWT_SECRET: string;
+      SERVER_PORT: string;
+      APP_PORT: string;
+      ORIGINS: string;
+    }
   }
 }
+
+export {};
